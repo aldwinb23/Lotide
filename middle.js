d36a5f28022365ee26e0d24fcda9c40ff7a58136
aldwinb <aldwinb23@gmail.com>
2022-09-21 15:17:48 -0700

    changed console.logs to assert

--- middle.js
+++ middle.js
@@ -38,10 +38,10 @@ const middle = function(array) {
   }
 };
 
-// console.log(middle([1]));
-// console.log(middle([1, 2]));
-// console.log(middle([1, 2, 3, 4]));
-// console.log(middle([1, 2, 3, 4, 5]));
-// console.log(middle([1, 2, 3, 4, 5, 6, 7, 8]));
-// console.log(middle([1, 2, 3, 4, 5, 6, 7, 8, 9]));
-// console.log(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]));
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3, 4]) , [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9]), [5]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]), [7, 8]);
