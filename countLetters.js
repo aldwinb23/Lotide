b07466184c5aabd0ef74503360e72322a81bffa7
aldwinb <aldwinb23@gmail.com>
2022-09-21 20:21:43 -0700

    initial commit

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✓✓✓ Assertion Passed: ${actual} === ${expected} ✓✓✓`);
+  } else if (actual !== expected) {
+    console.log(`✖︎✖︎✖︎ Assertion Failed: ${actual} !== ${expected} ✖︎✖︎✖︎`);
+  }
+};
+
+const countLetters = function(letters) {
+  let results = {};
+  for (const letter of letters) {
+    if (letter === " ") {
+      continue;
+    }
+    if (results[letter] === undefined) {
+      results[letter] = 0;
+    }
+    results[letter]++;
+  }
+  return results;
+};
+
+console.log(countLetters("lighthouse in the house"));
