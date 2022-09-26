870b5d1f46c3a630caf0de163a8a53f9d1222d87
aldwinb <aldwinb23@gmail.com>
2022-09-26 16:01:18 -0700

    refactored assertEqual and head

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,10 @@
 
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`✓✓✓ Assertion Passed: ${actual} === ${expected} ✓✓✓`);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected} ✅✅✅`);
   } else if (actual !== expected) {
-    console.log(`✖︎✖︎✖︎ Assertion Failed: ${actual} !== ${expected} ✖︎✖︎✖︎`);
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected} ❌❌❌`);
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+module.exports = assertEqual;
