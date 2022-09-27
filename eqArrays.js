9f90dbd175bb2a80e346c90c32e08a420b2ad036
aldwinb <aldwinb23@gmail.com>
2022-09-26 17:01:24 -0700

    refactored eqArrays and created eqArraysTest

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,4 @@
 
-const assertEqual = function(actual, expected) {
-  if (actual == expected) {
-    console.log(`✓✓✓ Assertion Passed: ${actual} === ${expected} ✓✓✓`);
-  } else if (actual != expected) {
-    console.log(`✖︎✖︎✖︎ Assertion Failed: ${actual} !== ${expected} ✖︎✖︎✖︎`);
-  }
-};
 const eqArrays = function(arrOne, arrTwo) {
   if (arrOne.length !== arrTwo.length) {
     return false;
@@ -17,7 +10,5 @@ const eqArrays = function(arrOne, arrTwo) {
   }
   return true;
 };
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+
+module.exports = eqArrays;
